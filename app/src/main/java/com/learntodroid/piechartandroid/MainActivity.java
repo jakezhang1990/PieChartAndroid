a6693758144724092052bdf220e66d3a10067d25
JakeZhang <2249692640@qq.com>
2021-04-11 16:26:33 +0800

    效果图

--- app/src/main/java/com/learntodroid/piechartandroid/MainActivity.java
+++ app/src/main/java/com/learntodroid/piechartandroid/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
@@ -29,21 +30,55 @@ public class MainActivity extends AppCompatActivity {
         loadPieChartData();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /*new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                loadPieChartData2();
+            }
+        },5000);*/
+    }
+
     private void setupPieChart() {
-        pieChart.setDrawHoleEnabled(true);
+        pieChart.setExtraOffsets(0f,45f,0f,45f);
         pieChart.setUsePercentValues(true);
         pieChart.setEntryLabelTextSize(12);
-        pieChart.setEntryLabelColor(Color.BLACK);
-        pieChart.setCenterText("Spending by Category");
-        pieChart.setCenterTextSize(24);
-        pieChart.getDescription().setEnabled(false);
+        pieChart.setEntryLabelColor(Color.RED);
 
-        Legend l = pieChart.getLegend();
+        pieChart.setDrawHoleEnabled(false);
+
+        pieChart.setRotationAngle(0); //设置初始旋转角为0度
+
+        // 触摸旋转
+        pieChart.setRotationEnabled(false);
+
+        /*pieChart.setCenterText("Spending by Category");
+        pieChart.setCenterTextSize(24);*/
+
+
+        pieChart.getDescription().setEnabled(true);
+
+        /*Legend l = pieChart.getLegend();
         l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
         l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
         l.setOrientation(Legend.LegendOrientation.VERTICAL);
         l.setDrawInside(false);
-        l.setEnabled(true);
+        l.setEnabled(true);*/
+
+        Legend legend = pieChart.getLegend();
+        //设置比例块换行...
+        legend.setWordWrapEnabled(true);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);//图例水平居中
+//        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);  //设置图例水平显示
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM); //底部
+        legend.setDirection(Legend.LegendDirection.LEFT_TO_RIGHT);
+//        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT); //左侧对其
+        legend.setTextColor(Color.RED); //图例文字的颜色
+        legend.setFormSize(15f);
+        legend.setXEntrySpace(12f);
+        legend.setTextSize(14f);  //图例文字的大小
     }
 
     private void loadPieChartData() {
@@ -53,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
         entries.add(new PieEntry(0.10f, "Entertainment"));
         entries.add(new PieEntry(0.25f, "Electricity and Gas"));
         entries.add(new PieEntry(0.3f, "Housing"));
+//        float value = pieChart.isUsePercentValuesEnabled() ? entry.getY() / yValueSum * 100f : entry.getY();
 
         ArrayList<Integer> colors = new ArrayList<>();
         for (int color: ColorTemplate.MATERIAL_COLORS) {
@@ -65,6 +101,67 @@ public class MainActivity extends AppCompatActivity {
 
         PieDataSet dataSet = new PieDataSet(entries, "Expense Category");
         dataSet.setColors(colors);
+        dataSet.setSliceSpace(3f);
+
+        PieData data = new PieData(dataSet);
+        data.setDrawValues(true);
+        data.setValueFormatter(new PercentFormatter(pieChart));
+//        data.setValueFormatter(new );
+        data.setValueTextSize(20f);
+        data.setValueTextColor(Color.YELLOW);
+
+        pieChart.setDrawEntryLabels(true);//只显示百分比不显示文字
+
+
+
+        pieChart.highlightValues(null);// 撤销所有的亮点
+        pieChart.setData(data);
+        pieChart.invalidate();
+
+/*
+
+        pieData.setDrawValues(true);//饼状图上显示值
+        dataSet.setValueLinePart1Length(0.3f);//设置连接线的长度
+//x,y值在圆外显示(在圆外才会有连接线)
+        dataSet.setXValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+        dataSet.setXValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+//数据连接线距图形片内部边界的距离，为百分数(0~100f)
+        dataSet.setValueLinePart1OffsetPercentage(0f);
+//设置x,y在圆外显示的值为透明(transparent = 0x00000000)
+        pieData.setValueTextColor(transparent);
+*/
+
+
+        dataSet.setValueLinePart1OffsetPercentage(60.0f);
+        dataSet.setValueLinePart1Length(0.8f);
+        dataSet.setValueLinePart2Length(0.9f);
+//        dataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+        dataSet.setValueTextColor(getResources().getColor(R.color.colorPrimary));
+
+//文字在饼图外面显示
+        dataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+        dataSet.setXValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+
+        pieChart.animateY(1400, Easing.EaseInOutQuad);
+    }
+
+    private void loadPieChartData2() {
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(0.5f, "aaa"));
+        entries.add(new PieEntry(0.5f, "bbb"));
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int color: ColorTemplate.MATERIAL_COLORS) {
+            colors.add(color);
+        }
+
+        for (int color: ColorTemplate.VORDIPLOM_COLORS) {
+            colors.add(color);
+        }
+
+        PieDataSet dataSet = new PieDataSet(entries, "Expense Category");
+        dataSet.setColors(colors);
+        dataSet.setSliceSpace(3f);
 
         PieData data = new PieData(dataSet);
         data.setDrawValues(true);
@@ -75,6 +172,6 @@ public class MainActivity extends AppCompatActivity {
         pieChart.setData(data);
         pieChart.invalidate();
 
-        pieChart.animateY(1400, Easing.EaseInOutQuad);
+        pieChart.animateY(1000, Easing.EaseInOutQuad);
     }
 }
